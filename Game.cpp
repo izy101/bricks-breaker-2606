1dc46b58b91d37fd7415623cd026158e08d9a56e
Izabella <izabellasappgonzalez@gmail.com>
2026-07-01 21:28:45 -0400

    Todo #5: Removes brick after three hits

--- Game.cpp
+++ Game.cpp
@@ -85,14 +85,18 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	for (Box& brick : bricks){
+	for (int i = 0; i < bricks.size(); ++i) {
+		Box& brick = bricks[i];
 		if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
 		{
 			brick.color = ConsoleColor(brick.color - 1);
 			ball.y_velocity *= -1;
 
 			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-
+			if (bricks[i].color == ConsoleColor::Black) {
+				bricks.erase(bricks.begin() + i);
+			}
+			break;
 		}
 }
 
